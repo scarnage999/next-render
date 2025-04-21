62bfcbf31922a82d3dc280434ebb33701ef68091
macbook <scarnage.999@gmail.com>
2025-04-21 13:12:23 -0700

    change color topBar

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -1,6 +1,6 @@
 export const TopBar = () => {
     return (
-    <div className="bg-slate-900 text-white p-5 text-center">
+    <div className="bg-blue-400 text-white p-5 text-center">
         <h1>Proyecto varios tipos</h1>
     </div>
     )
